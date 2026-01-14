80f86583d40ef59f41f94e2b2b2ff690b3bbcd64
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-01-14 10:24:06 -0300

    Adiciona a classe Usuario que estende UsuarioBase com construtor

---
+++ src/br/com/corrida/model/Usuario.java
@@ -0,0 +1,8 @@
+package br.com.corrida.model;
+
+public class Usuario extends UsuarioBase {
+    // Construtor
+    public Usuario(int id, String nome, String telefone) {
+        super(id, nome, telefone);
+    }
+}
