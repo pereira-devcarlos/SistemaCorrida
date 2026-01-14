778e2a06cdc58a62896d08a257f5654b8fd5abc4
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-01-14 10:23:32 -0300

    Adiciona atributos, construtor e métodos getters e setters à classe UsuarioBase

--- src/br/com/corrida/model/UsuarioBase.java
+++ src/br/com/corrida/model/UsuarioBase.java
@@ -1,7 +1,35 @@
 package br.com.corrida.model;
 
 public abstract class UsuarioBase {
+    // Atributos
     private int id;
     private String nome;
     private String telefone;
+
+    // Construtor
+    public UsuarioBase(int id, String nome, String telefone) {
+        this.id = id;
+        this.nome = nome;
+        this.telefone = telefone;
+    }
+
+    // Getters e Setters
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public String getNome() {
+        return nome;
+    }
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    public String getTelefone() {
+        return telefone;
+    }
+    public void setTelefone(String telefone) {
+        this.telefone = telefone;
+    }
 }
