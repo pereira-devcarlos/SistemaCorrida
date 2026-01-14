3ea0cea24137b90fa167b5ac679f1f0129755d94
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-01-14 10:58:29 -0300

    Adiciona as classes UsuarioRepository e UsuarioService com estrutura básica

---
+++ src/br/com/corrida/repository/UsuarioRepository.java
@@ -0,0 +1,7 @@
+package br.com.corrida.repository;
+
+import br.com.corrida.model.Usuario;
+
+public class UsuarioRepository {
+    
+}
---
+++ src/br/com/corrida/service/UsuarioService.java
@@ -0,0 +1,8 @@
+package br.com.corrida.service;
+
+import br.com.corrida.model.Usuario;
+import br.com.corrida.repository.UsuarioRepository;
+
+public class UsuarioService {
+    
+}
