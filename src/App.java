027000aa7cf40be713742515350d34a9236727da
pereira-devcarlos <contato.devcarlos@gmail.com>
2026-01-14 10:11:39 -0300

    Adiciona configuração do projeto Java e implementa classe principal com mensagens de boas-vindas

---
+++ src/App.java
@@ -0,0 +1,6 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("\tOlá usuario!");
+        System.out.println("Seja bem-vindo ao moto uber!");
+    }
+}
